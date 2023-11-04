5edca832f0e133eeca1103c1884d4e4e82b45820
Jordan Quartey <jordev14@gmail.com>
2023-11-04 23:45:32 +0000

    chore: set up fonts file

---
+++ src/app/fonts.ts
@@ -0,0 +1,5 @@
+import { Kumbh_Sans, Quicksand, Urbanist } from 'next/font/google'
+
+export const body = Urbanist({ subsets: ['latin'] })
+export const heading = Quicksand({ subsets: ['latin'] })
+export const logo = Kumbh_Sans({ subsets: ['latin'] })
